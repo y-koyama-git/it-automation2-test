76df962bcb997f9a9ff08def7be11efc61f185fd
ta-matsuo <taka-matsuo@ncontr.com>
2022-06-21 05:56:32 +0000

    ita_api_admin

---
+++ ita_root/ita_api_admin/controllers/organization_create_controller.py
@@ -0,0 +1,33 @@
+#   Copyright 2022 NEC Corporation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import connexion
+import six
+
+
+def organization_create(body, organization_id):  # noqa: E501
+    """organization_create
+
+    Organizationを作成する # noqa: E501
+
+    :param body: 
+    :type body: dict | bytes
+    :param organization_id: Organization名
+    :type organization_id: str
+
+    :rtype: InlineResponse200
+    """
+    if connexion.request.is_json:
+        body = object.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
---
+++ ita_root/ita_api_admin/sql/organization.sql
@@ -0,0 +1,19 @@
+-- WorkspaceDB管理
+CREATE TABLE T_COMN_WORKSPACE_DB_INFO
+(
+    PRIMARY_KEY                     VARCHAR(40),                                -- 主キー
+    DB_HOST                         VARCHAR(255),                               -- ホスト
+    DB_PORT                         INT,                                        -- ポート
+    DB_DATADBASE                    VARCHAR(255),                               -- DB名
+    DB_USER                         VARCHAR(255),                               -- ユーザ
+    DB_PASSWORD                     VARCHAR(255),                               -- パスワード
+    NOTE                            TEXT,                                       -- 備考
+    DISUSE_FLAG                     VARCHAR(1)  ,                               -- 廃止フラグ
+    LAST_UPDATE_TIMESTAMP           DATETIME(6),                                -- 最終更新日時
+    LAST_UPDATE_USER                VARCHAR(40),                                -- 最終更新者
+    PRIMARY KEY(PRIMARY_KEY)
+)ENGINE = InnoDB, CHARSET = utf8mb4, COLLATE = utf8mb4_bin, ROW_FORMAT=COMPRESSED ,KEY_BLOCK_SIZE=8;
+
+
+
+
