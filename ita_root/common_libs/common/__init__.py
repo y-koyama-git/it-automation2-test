44687d6c76cf1ad83908b42442bb42b2643ef636
uekawa-tomonari <16901713+uekawa-tomonari@users.noreply.github.com>
2022-06-21 01:56:55 +0000

    workspace_create_controller.pyの作成とDB周りの修正 ・DBにrootアクセス可能なクラスを作成 ・sql文字列の中の%をpreparedしない場合はエスケープ

--- ita_root/common_libs/common/__init__.py
+++ ita_root/common_libs/common/__init__.py
@@ -1,3 +1,3 @@
-from .logger import app
-from .dbconnect import DBConnectOrg, DBConnectWs
-from .message_class import MessageTemplate
+from .logger import app  # noqa: F401
+from .dbconnect import DBConnectOrg, DBConnectWs  # noqa: F401
+from .message_class import MessageTemplate  # noqa: F401
--- ita_root/common_libs/common/dbconnect.py
+++ ita_root/common_libs/common/dbconnect.py
@@ -2,16 +2,15 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 """
 database connection agent module
 """
@@ -20,45 +19,42 @@ import pymysql.cursors  # https://pymysql.readthedocs.io/en/latable_name/
 import os
 import uuid
 import datetime
+# for generate password
+import secrets
+import string
+import re
 
 
 class DBConnectWs:
     """
     database connection agnet class for workspace-db on mariadb
     """
-    # info for connection
-    # _db = ""
-    # _host = ""
-    # _port = ""
-    # _db_user = ""
-    # _db_passwd = ""
-    
-    __workspace_name = ""
+
+    _workspace_id = ""
 
     _db_con = None  # database connection
 
     _is_transaction = False  # state of transaction
     _COLUMN_NAME_TIMESTAMP = 'LAST_UPDATE_TIMESTAMP'
 
-    def __init__(self, workspace_name):
+    def __init__(self, workspace_id):
         """
         constructor
 
         Arguments:
-            workspace_name: workspace name
+            workspace_id: workspace id
         """
-        # decide database name, prefix+workspace_name
-        self.__workspace_name = workspace_name
-        self._db = "WSDB_" + self.__workspace_name.upper()
+        # decide database name, prefix+workspace_id
+        self._workspace_id = workspace_id
+        self._db = self.get_wsdb_name(workspace_id)
 
         # get db-connect-infomation from organization-db
-        db_org = DBConnectOrg()
-        connect_info = db_org.table_select("T_COMN_WORKSPACE_DB_INFO", "WHERE `DB_DATADBASE` = %s", [self._db])
-        if len(connect_info) == 0:
+        org_db = DBConnectOrg()
+        connect_info = org_db.get_wsdb_connect_info(self._db)
+        if connect_info is False:
             msg = "Dabase Connect Error db_name={} : Database is not found".format(self._db)
             raise Exception(msg)
 
-        connect_info = connect_info[0]
         self._host = connect_info['DB_HOST']
         self._port = connect_info['DB_PORT']
         self._db_user = connect_info['DB_USER']
@@ -78,7 +74,7 @@ class DBConnectWs:
         connect database
 
         Returns:
-            is success:(bool) 
+            is success:(bool)
         """
         if self._db_con is not None and self._db_con.open is True:
             return True
@@ -114,7 +110,7 @@ class DBConnectWs:
         begin
 
         Returns:
-            is success:(bool) 
+            is success:(bool)
         """
         res = False
         if self._db_con.open is True and self._is_transaction is False:
@@ -138,7 +134,7 @@ class DBConnectWs:
         Arguments:
             flg: True(1):commit, False(0):rollback
         Returns:
-            is success:(bool) 
+            is success:(bool)
         """
         res = False
         if self._db_con.open is True:
@@ -153,7 +149,7 @@ class DBConnectWs:
         commit
 
         Returns:
-            is success:(bool) 
+            is success:(bool)
         """
         res = False
         if self._db_con.open is True and self._is_transaction is True:
@@ -175,7 +171,7 @@ class DBConnectWs:
         rollback
 
         Returns:
-            is success:(bool) 
+            is success:(bool)
         """
         res = False
         if self._db_con.open is True and self._is_transaction is True:
@@ -190,20 +186,41 @@ class DBConnectWs:
                 self._is_transaction = False
         return res
 
+    def sqlfile_execute(self, file_name):
+        """
+        read sql file and execute sql
+
+        Arguments:
+            file_name: sql file path
+        """
+        with open(file_name, "r", encoding='utf-8-sig') as f:
+            sql_list = f.read().split(";\n")
+            for sql in sql_list:
+                if re.fullmatch(r'[\s\n\r]*', sql) is None:
+                    self.sql_execute(sql)
+
     def sql_execute(self, sql, bind_value_list=[]):
         """
         execute sql
 
         Arguments:
             sql: sql statement ex."SELECT * FROM table_name WHERE name = %s and number = %s"
-            bind_value_list: value list ex.["hoge taro", 5]
+            bind_value_list: list or tuple or dict ex.["hoge taro", 5]
         Returns:
             table data list: list(tuple)
         """
         db_cursor = self._db_con.cursor()
 
+        # escape
+        if len(bind_value_list) == 0:
+            sql = self.prepared_val_escape(sql)
+        else:
+            # convert list→tupple
+            if isinstance(bind_value_list, list):
+                bind_value_list = tuple(bind_value_list)
+
         try:
-            db_cursor.execute(sql, tuple(bind_value_list))
+            db_cursor.execute(sql, bind_value_list)
         except pymysql.Error as e:
             msg = "SQL Error : db_name={}, sql='{}', bind_value={} : {}".format(self._db, sql, bind_value_list, e)
             raise Exception(msg)
@@ -224,7 +241,7 @@ class DBConnectWs:
                 column name list: ex.['id', 'name', 'number']
                 primary_key list: ex.['id']
         """
-        sql = "SHOW COLUMNS FROM {}".format(table_name)
+        sql = "SHOW COLUMNS FROM `{}`".format(table_name)
         data_list = self.sql_execute(sql)
 
         column_list = []
@@ -266,7 +283,7 @@ class DBConnectWs:
             data count: int
         """
         where_str = " {}".format(where_str) if where_str else ""
-        sql = "SELECT COUNT(*) FROM {}{}".format(table_name, where_str)
+        sql = "SELECT COUNT(*) FROM `{}`{}".format(table_name, where_str)
         data_list = self.sql_execute(sql, bind_value_list)
 
         for data in data_list:
@@ -288,6 +305,8 @@ class DBConnectWs:
         """
         if isinstance(data_list, dict):
             data_list = [data_list]
+
+        self.db_transaction_start()
 
         is_last_res = True
         for data in data_list:
@@ -314,7 +333,7 @@ class DBConnectWs:
                 continue
             # insert history table
             history_table_name = table_name + "_JNL"
-            add_data = self._make_history_table_data("INSERT", timestamp)
+            add_data = self._get_history_table_data("INSERT", timestamp)
             # make history data
             history_data = dict(data, **add_data)
 
@@ -350,6 +369,8 @@ class DBConnectWs:
         if isinstance(data_list, dict):
             data_list = [data_list]
 
+        self.db_transaction_start()
+
         is_last_res = True
         for data in data_list:
             # auto set
@@ -374,7 +395,7 @@ class DBConnectWs:
                 continue
             # insert history table
             history_table_name = table_name + "_JNL"
-            add_data = self._make_history_table_data("UPDATE", timestamp)
+            add_data = self._get_history_table_data("UPDATE", timestamp)
 
             # re-get all column data
             data = self.table_select(table_name, "WHERE `{}` = '{}'".format(primary_key_name, primary_key_value))
@@ -407,7 +428,7 @@ class DBConnectWs:
         Arguments:
             table_name_list: table_name list
         Returns:
-            is success:(bool) 
+            is success:(bool)
         """
         if isinstance(table_name_list, str):
             table_name_list = [table_name_list]
@@ -419,15 +440,54 @@ class DBConnectWs:
         res = self.sql_execute(sql, table_name_list)
         return res
 
+    def prepared_val_escape(self, val):
+        """
+        escape sql statement
+        
+        Arguments:
+            val: str
+        Returns:
+            val: str
+        """
+        if isinstance(val, str):
+            return val.replace("%", "%%")
+
+        return val
+
+    def prepared_list_escape(self, str_list):
+        """
+        escape list of sql statement
+        
+        Arguments:
+            str_list: list of str
+        Returns:
+            str_list: list of str
+        """
+        return list(map(lambda s: self.prepared_val_escape(s), str_list))
+
+    def get_wsdb_name(self, workspace_id):
+        """
+        get database name for workspace
+        
+        Arguments:
+            workspace_id: workspace_id
+        Returns:
+            database name for workspace: str
+        """
+        return "WSDB_" + workspace_id.upper()
+
     def _uuid_create(self):
         """
         make uuid of version4
+        
+        Returns:
+            uuid
         """
         return uuid.uuid4()
 
-    def _make_history_table_data(self, action_class, timestamp):
+    def _get_history_table_data(self, action_class, timestamp):
         """
-        make addtinal data for history JNL table
+        get addtinal data for history JNL table
 
         Arguments:
             action_class: "INSERT" or "UPDATE"
@@ -472,7 +532,149 @@ class DBConnectOrg(DBConnectWs):
     def table_update(self, table_name, data_list, primary_key_name, is_register_history=False):
         return super().table_update(table_name, data_list, primary_key_name, is_register_history)
 
+    def get_wsdb_connect_info(self, db_name):
+        """
+        get database connect infomation for workspace
+        
+        Arguments:
+            db_name: database name
+        Returns:
+            database connect infomation for workspace: dict
+            or
+            get failure: (bool)False
+        """
+        data_list = self.table_select("T_COMN_WORKSPACE_DB_INFO", "WHERE `DB_DATADBASE` = %s", [db_name])
 
-if __name__ == '__main__':
-    pass
+        if len(data_list) == 0:
+            return False
 
+        return data_list[0]
+
+
+class DBConnectOrgRoot(DBConnectOrg):
+    """
+    database connection agnet class for organization-db on mariadb
+    """
+
+    def __init__(self):
+        """
+        constructor
+        """
+        self._db = "None"
+        self._host = os.environ.get('DB_HOST')
+        self._port = int(os.environ.get('DB_PORT'))
+        self._db_user = 'root'
+        self._db_passwd = os.environ.get('DB_ROOT_PASSWORD')
+
+        # connect database
+        self.db_connect()
+
+    def __del__(self):
+        """
+        destructor
+        """
+        self.db_disconnect()
+
+    def db_connect(self):
+        """
+        connect database
+
+        Returns:
+            is success:(bool)
+        """
+        if self._db_con is not None and self._db_con.open is True:
+            return True
+
+        try:
+            self._db_con = pymysql.connect(
+                host=self._host,
+                port=self._port,
+                user=self._db_user,
+                passwd=self._db_passwd,
+                charset='utf8',
+                cursorclass=pymysql.cursors.DictCursor
+            )
+        except pymysql.Error as e:
+            msg = "Dabase Connect Error: {}".format(e)
+            raise Exception(msg)
+
+        return True
+
+    def database_create(self, db_name):
+        """
+        create database
+        """
+        self.database_drop(db_name)
+
+        sql = "CREATE DATABASE `{}` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci".format(db_name)
+        self.sql_execute(sql)
+
+    def database_drop(self, db_name):
+        """
+        drop database
+        """
+        sql = "DROP DATABASE IF EXISTS `{}`".format(db_name)
+        self.sql_execute(sql)
+
+    def user_create_ws(self, db_name):
+        """
+        create user for workspace
+        
+        Arguments:
+            db_name: database name
+        Returns:
+            user_name and db_name: tuple
+        """
+        user_name = re.sub(r'^WSDB_', 'WSUSER_', db_name)
+        return self.user_create(user_name, db_name)
+
+    def user_create(self, user_name, db_name):
+        """
+        create user
+        
+        Arguments:
+            user_name: user name
+            db_name: database name
+        Returns:
+            user_name and db_name: tuple
+        """
+        self.user_drop(user_name)
+
+        user_password = self._password_generate()
+        sql = "CREATE USER IF NOT EXISTS '{user_name}'@'%' IDENTIFIED BY '{user_password}'".format(user_name=user_name, user_password=user_password)
+        self.sql_execute(sql)
+
+        sql = "GRANT ALL PRIVILEGES ON `{db_name}`.* TO '{user_name}'@'%'".format(user_name=user_name, db_name=db_name)
+        self.sql_execute(sql)
+
+        return user_name, user_password
+
+    def user_drop(self, user_name):
+        """
+        drop user
+        """
+        sql = "DROP USER IF EXISTS '{}'@'%'".format(user_name)
+        self.sql_execute(sql)
+
+    def _password_generate(self, escape=True):
+        """
+        generate password
+        
+        Arguments:
+            escape or not: bool
+        Returns:
+            password string: str
+        """
+        length = 16
+        # string.ascii_letters - alfabet lower and upper
+        # string.digits - number
+        # string.punctuation - symbol  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+        mysql_available_symbol = "!#%&()*+,-./;<=>?@[]^_{|}~"
+        pass_chars = string.ascii_letters + string.digits + mysql_available_symbol
+
+        password = ''.join(secrets.choice(pass_chars) for x in range(length))
+
+        if escape is True:
+            password = re.sub(r'([{})/g'.format(mysql_available_symbol), r'\$1', password)
+
+        return password
