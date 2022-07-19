d340fa4ab2b1cef1a27a86fd32fc5fe449c3ad5b
y-yonekura <y-yonekura@ncontr.com>
2022-07-19 11:53:35 +0000

    loadtable 個別処理

--- ita_root/common_libs/validate/valid_xxxxx-1001.py
+++ ita_root/common_libs/validate/valid_xxxxx-1001.py
@@ -1,28 +1,32 @@
-
-def external_valid_before(objdbca, objtable, val, parameter, option):
+import pprint
+def external_valid_before(objdbca, objtable, option):
     print('xxxxx-1001')
     print('external_valid_before')
     retBool = True
     msg = ''
-    return retBool, msg, val, parameter, option,
+    pprint.pprint(option)
+    return retBool, msg, option,
 
-def external_valid_after(objdbca, objtable, val, parameter, option):
+def external_valid_after(objdbca, objtable, option):
     print('xxxxx-1001')
     print('external_valid_after')
     retBool = True
     msg = ''
-    return retBool, msg, val, parameter, option,
+    pprint.pprint(option)
+    return retBool, msg, option,
 
-def external_valid_menu_before(objdbca, objtable, parameter, file, option):
+def external_valid_menu_before(objdbca, objtable, option):
     print('xxxxx-1001')
     print('external_valid_menu_before')
     retBool = True
     msg = ''
-    return retBool, msg, parameter, file, option,
+    pprint.pprint(option)
+    return retBool, msg, option,
 
-def external_valid_menu_after(objdbca, objtable, parameter, file, option):
+def external_valid_menu_after(objdbca, objtable, option):
     print('xxxxx-1001')
     print('external_valid_menu_after')
     retBool = True
     msg = ''
-    return retBool, msg, parameter, file, option,
+    pprint.pprint(option)
+    return retBool, msg, option,
