5c1d9cb21293d4a0261c3a20333f0c22680226d9
uekawa-tomonari <16901713+uekawa-tomonari@users.noreply.github.com>
2022-07-22 07:43:22 +0000

    コンテナ微調整

--- ita_root/common_libs/ansible_driver/classes/__init__.py
+++ ita_root/common_libs/ansible_driver/classes/__init__.py
@@ -1,6 +1,4 @@
 from .AnscConstClass import AnscConst  # noqa F401
-from .AnslConstClass import AnslConst  # noqa F401
-from .AnspConstClass import AnspConst  # noqa F401
 from .AnsrConstClass import AnsrConst  # noqa F401
 from .WrappedStringReplaceAdmin import WrappedStringReplaceAdmin  # noqa F401
 from .YamlParseClass import YamlParse  # noqa F401
