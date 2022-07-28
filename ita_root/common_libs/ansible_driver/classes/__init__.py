839c4ba8ee77bad14f1d5e2233eea8b64b1e0c6b
enoman <c_man_enomoto@ncontr.com>
2022-07-28 05:00:21 +0000

    新規

--- ita_root/common_libs/ansible_driver/classes/__init__.py
+++ ita_root/common_libs/ansible_driver/classes/__init__.py
@@ -4,3 +4,4 @@ from .WrappedStringReplaceAdmin import WrappedStringReplaceAdmin  # noqa F401
 from .YamlParseClass import YamlParse  # noqa F401
 from .VarStructAnalJsonConvClass import VarStructAnalJsonConv  # noqa F401
 from .gitlab import GitLabAgent  # noqa F401
+from .AnsibleVaultClass import AnsibleVault
