84409289cf214bae7fc90a60736df750467f572d
enoman <c_man_enomoto@ncontr.com>
2022-09-15 23:55:53 +0000

    修正

--- ita_root/ita_api_admin/sql/ansible.sql
+++ ita_root/ita_api_admin/sql/ansible.sql
@@ -303,6 +303,34 @@ CREATE TABLE T_ANSC_COMVRAS_USLIST
 
 
 
+-- 20108 管理対象外変数一覧
+CREATE TABLE T_ANSC_UNMANAGED_VARLIST
+(
+    ROW_ID                          VARCHAR(40),                                -- 項番
+    VAR_NAME                        VARCHAR(255),                               -- 変数名
+    NOTE                            TEXT,                                       -- 備考
+    DISUSE_FLAG                     VARCHAR(1),                                 -- 廃止フラグ
+    LAST_UPDATE_TIMESTAMP           DATETIME(6),                                -- 最終更新日時
+    LAST_UPDATE_USER                VARCHAR(40),                                -- 最終更新者
+    PRIMARY KEY(ROW_ID)
+)ENGINE = InnoDB, CHARSET = utf8mb4, COLLATE = utf8mb4_bin, ROW_FORMAT=COMPRESSED ,KEY_BLOCK_SIZE=8;
+
+CREATE TABLE T_ANSC_UNMANAGED_VARLIST_JNL
+(
+    JOURNAL_SEQ_NO                  VARCHAR(40),                                -- 履歴用シーケンス
+    JOURNAL_REG_DATETIME            DATETIME(6),                                -- 履歴用変更日時
+    JOURNAL_ACTION_CLASS            VARCHAR (8),                                -- 履歴用変更種別
+    ROW_ID                          VARCHAR(40),                                -- 項番
+    VAR_NAME                        VARCHAR(255),                               -- 変数名
+    NOTE                            TEXT,                                       -- 備考
+    DISUSE_FLAG                     VARCHAR(1),                                 -- 廃止フラグ
+    LAST_UPDATE_TIMESTAMP           DATETIME(6),                                -- 最終更新日時
+    LAST_UPDATE_USER                VARCHAR(40),                                -- 最終更新者
+    PRIMARY KEY(JOURNAL_SEQ_NO)
+)ENGINE = InnoDB, CHARSET = utf8mb4, COLLATE = utf8mb4_bin, ROW_FORMAT=COMPRESSED ,KEY_BLOCK_SIZE=8;
+
+
+
 -- 20401 Role ロール名管理
 CREATE TABLE T_ANSR_ROLE_NAME
 (
