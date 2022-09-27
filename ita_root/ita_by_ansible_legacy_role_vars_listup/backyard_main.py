0401c6df5bc0e104c36a499541df245bdf40c772
yoshiyuki-konuma <yoshiyuki-konuma@ncontr.com>
2022-09-27 15:20:31 +0900

    listup-workflow 実行判定追加

--- ita_root/ita_by_ansible_legacy_role_vars_listup/backyard_main.py
+++ ita_root/ita_by_ansible_legacy_role_vars_listup/backyard_main.py
@@ -13,8 +13,7 @@
 #
 from flask import g
 from common_libs.common.dbconnect import *  # noqa: F403
-from common_libs.common.exception import AppException
-from backyard_libs.ansible_driver.classes import *
+from backyard_libs.ansible_driver.classes import *  # noqa: F403
 from backyard_libs.ansible_driver.functions import util
 
 
@@ -32,36 +31,52 @@ def backyard_main(organization_id, workspace_id):
     # if 'USER_ID' not in g:
     g.USER_ID = '20401'
 
+    proc_loaded_row_id = 204
+
     # DB接続準備
     ws_db = DBConnectWs(workspace_id)  # noqa: F405
 
     # 関連データベースが更新されバックヤード処理が必要か判定
-    # if not has_changes_related_tables(ws_db):
-    #     print("no changes, skip workflow.")
-    #     return
+    if not has_changes_related_tables(ws_db, proc_loaded_row_id):
+        g.applogger.debug("No changes, skip workflow.")
+        return
 
     # 各インスタンス準備
-    tpl_table = TemplateTable(ws_db)
+    g.applogger.debug("Read all related table.")
+    tpl_table = TemplateTable(ws_db)  # noqa: F405
     tpl_table.store_dbdata_in_memory()
 
-    device_table = DeviceTable(ws_db)
+    device_table = DeviceTable(ws_db)  # noqa: F405
     device_table.store_dbdata_in_memory()
 
-    role_pkg_table = RolePkgTable(ws_db)
+    role_pkg_table = RolePkgTable(ws_db)  # noqa: F405
     role_pkg_table.store_dbdata_in_memory()
 
-    role_name_table = RoleNameTable(ws_db)
+    role_name_table = RoleNameTable(ws_db)  # noqa: F405
     role_name_table.store_dbdata_in_memory(contain_disused_data=True)
 
-    mov_table = MovementTable(ws_db)
+    mov_table = MovementTable(ws_db)  # noqa: F405
     mov_table.store_dbdata_in_memory()
 
-    mov_material_link_table = MovementMaterialLinkTable(ws_db)
+    mov_material_link_table = MovementMaterialLinkTable(ws_db)  # noqa: F405
     mov_material_link_table.store_dbdata_in_memory()
+
+    mov_vars_link_table = MovementVarsLinkTable(ws_db)  # noqa: F405
+    mov_vars_link_table.store_dbdata_in_memory(contain_disused_data=True)
+
+    nest_vars_mem_table = NestVarsMemberTable(ws_db)  # noqa: F405
+    nest_vars_mem_table.store_dbdata_in_memory(contain_disused_data=True)
+
+    mem_max_col_table = NestVarsMemberMaxColTable(ws_db)  # noqa: F405
+    mem_max_col_table.store_dbdata_in_memory(contain_disused_data=True)
+
+    mem_col_comb_table = NestVarsMemberColCombTable(ws_db)  # noqa: F405
+    mem_col_comb_table.store_dbdata_in_memory(contain_disused_data=True)
 
     # - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - +
     # メイン処理開始
     # - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - +
+    g.applogger.debug("Read all related table.")
     # RolePKG - Role 変数チェック
     role_name_list, role_varmgr_dict = role_pkg_table.extract_variable()
 
@@ -81,26 +96,18 @@ def backyard_main(organization_id, workspace_id):
     mov_vars_dict = util.extract_variable_for_execute(mov_vars_dict, tpl_varmng_dict, device_varmng_dict, ws_db)
 
     # Movement変数 登録・廃止
-    mov_vars_link_table = MovementVarsLinkTable(ws_db)
-    mov_vars_link_table.store_dbdata_in_memory()
     mov_vars_link_table.register_and_discard(mov_vars_dict)
     registerd_mov_vars_link_records = mov_vars_link_table.get_stored_records()
 
     # メンバ変数 登録・廃止
-    nest_vars_mem_table = NestVarsMemberTable(ws_db)
-    nest_vars_mem_table.store_dbdata_in_memory()
     nest_vars_mem_table.register_and_discard(mov_vars_dict, registerd_mov_vars_link_records)
     registerd_nest_vars_mem_records = nest_vars_mem_table.get_stored_records()
 
     # 変数ネスト管理 登録・廃止
-    mem_max_col_table = NestVarsMemberMaxColTable(ws_db)
-    mem_max_col_table.store_dbdata_in_memory()
     mem_max_col_table.register_and_discard(registerd_nest_vars_mem_records)
     registerd_mem_max_col_records = mem_max_col_table.get_stored_records()
 
     # 多段変数配列組合せ管理管理 登録・廃止
-    mem_col_comb_table = NestVarsMemberColCombTable(ws_db)
-    mem_col_comb_table.store_dbdata_in_memory()
     nominate_mem_col_comb = util.expand_vars_member(registerd_nest_vars_mem_records, registerd_mem_max_col_records)
     mem_col_comb_table.register_and_discard(nominate_mem_col_comb)
 
@@ -115,10 +122,11 @@ def backyard_main(organization_id, workspace_id):
 
     print("backyard_main ita_by_ansible_legacy_role_vars_listup end")
 
-def has_changes_related_tables(ws_db):
+
+def has_changes_related_tables(ws_db, proc_loaded_row_id):
 
     where_str = " WHERE  ROW_ID = %s AND (LOADED_FLG is NULL OR LOADED_FLG <> '1')"
-    bind_value_list=[204]
+    bind_value_list = [proc_loaded_row_id]
     not_loaded_count = ws_db.table_count("T_COMN_PROC_LOADED_LIST", where_str, bind_value_list)
 
-    return ( not_loaded_count > 0 )
+    return (not_loaded_count > 0)
