1bb8d528ac2c8f85179417613e37d73722533fe7
h-nanri <hibiki.nanri@lean-jp.com>
2022-09-30 02:26:09 +0000

    「プルダウン検索」の除外対象に「FileUploadEncryptColumn」追加

--- ita_root/ita_api_organization/libs/menu_info.py
+++ ita_root/ita_api_organization/libs/menu_info.py
@@ -609,8 +609,8 @@ def collect_search_candidates(objdbca, menu, column, menu_record={}, menu_table_
     save_type = str(ret[0].get('SAVE_TYPE'))
     
     # パスワードカラム系の場合は499を返却
-    # 8(PasswordColumn), 15(MaskColumn), 16(SensitiveSingleTextColumn), 17(SensitiveMultiTextColumn), 25(PasswordIDColumn), 26(JsonPasswordIDColumn)
-    sensitive_column_list = ["8", "15", "16", "17", "25", "26"]
+    # 8(PasswordColumn), 15(MaskColumn), 16(SensitiveSingleTextColumn), 17(SensitiveMultiTextColumn), 20(FileUploadEncryptColumn), 25(PasswordIDColumn), 26(JsonPasswordIDColumn)
+    sensitive_column_list = ["8", "15", "16", "17", "20", "25", "26"]
     if column_class_id in sensitive_column_list:
         log_msg_args = [menu, column]
         api_msg_args = [menu, column]
